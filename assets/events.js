7ca0710f0329158ba1e202ebca1496cdd5651abe
Chanakyha <vetrichanakyha2003@gmail.com>
2023-03-08 13:04:04 +0530

    Events Description Page ✅

--- assets/events.js
+++ assets/events.js
@@ -5,9 +5,11 @@ export const technicalEvents = [
     clubName: "Slug & Plug",
     rules: "It's a coding event, that consists of 3 challenges.",
     staffCo: ["Mrs.C.Ashwini"],
-    studentCo: ["Pranav ,Spandan"],
+    studentCo: ["Pranav", "Spandan"],
     venue: "Admin 2nd floor Lab",
     date: "21-04-2023",
+    time: "9:00 AM",
+    entryFee: 25,
   },
   {
     id: 23562356238502,
@@ -16,8 +18,10 @@ export const technicalEvents = [
     rules:
       "A team coding competition, only two members will be allowed per team. Both members will work on a given problem to achieve the desired solution, by using the concept of Pair-Programming",
     staffCo: ["Dr.M.S.Antony Vigil", "Dr.M.S.Minu"],
-    studentCo: ["Mehul Tomar ,Yash Kanodia"],
+    studentCo: ["Mehul Tomar", "Yash Kanodia"],
     venue: "BMS Lab 4 and 5",
     date: "22-04-2023",
+    time: "1:00 PM",
+    entryFee: 50,
   },
 ];
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -9,7 +9,8 @@ const EventDescription = () => {
 
   const event = technicalEvents.find((event) => event.id == eventId);
   console.log(event);
-  if (!event) {
+
+  if (!event || event == undefined) {
     return <div>No Event Found</div>;
   }
 
@@ -17,24 +18,28 @@ const EventDescription = () => {
     <div className="min-h-screen w-full bg-black font-montserrat">
       <Image
         alt=""
-        className="p-20 pb-10"
-        src={require("../../assets/images/event-desc.png")}
+        className="lg:p-15 md:p-10 pb-5 w-full md:rounded-[3.5rem]"
+        src={require("../../assets/images/event-desc-blank.jpg")}
       />
 
-      <div className="p-20 pt-0 text-lg font-semibold">
+      <div className="lg:p-20 p-5 pt-0 lg:pt-0 text-lg font-semibold">
         <p className="text-[#FF6240] cursor-pointer underline underline-[#FF6240]">
           Back to all events
         </p>
         <div className="mt-5 flex justify-between items-center">
           <div>
-            <h1 className="text-white text-6xl">{event?.eventName}</h1>
-            <p className="text-[#747474] capitalize">By {event?.clubName}</p>
+            <h1 className="text-white text-2xl md:text-4xl lg:text-6xl">
+              {event?.eventName}
+            </h1>
+            <p className="text-[#747474] capitalize text-xs md:text-sm lg:text-lg">
+              By {event?.clubName}
+            </p>
           </div>
           <button className="bg-red-500 px-4 py-2 rounded-lg text-white">
             Register Now
           </button>
         </div>
-        <div className="flex justify-between my-10">
+        <div className="flex flex-col md:flex-row space-y-1 md:space-y-0 justify-between my-10">
           <div className="flex items-center space-x-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -57,34 +62,103 @@ const EventDescription = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              strokeWidth={1.5}
+              strokeWidth="1.5"
               stroke="currentColor"
               className="w-6 h-6 text-white"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
-            <p className="text-white font-light">{event?.date}</p>
+            <p className="text-white font-light">{event?.time}</p>
           </div>
           <div className="flex items-center space-x-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              strokeWidth={1.5}
+              strokeWidth="1.5"
               stroke="currentColor"
               className="w-6 h-6 text-white"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
               />
             </svg>
-            <p className="text-white font-light">{event?.date}</p>
+
+            <p className="text-white font-light">{event?.venue}</p>
+          </div>
+        </div>
+        <div className="my-10">
+          <h1 className="text-white text-3xl">Event Description</h1>
+          <p className="text-white font-light mt-3">
+            Voluptate consectetur aute excepteur est cillum nisi esse sit ad non
+            aliquip consectetur quis voluptate. Dolor mollit mollit elit
+            reprehenderit est. Tempor eu magna mollit officia ea aliquip
+            exercitation minim laborum anim veniam ut. Voluptate consectetur
+            aute excepteur est cillum nisi esse sit ad non aliquip consectetur
+            quis voluptate. Dolor mollit mollit elit reprehenderit est. Tempor
+            eu magna mollit officia ea aliquip exercitation minim laborum anim
+            veniam ut. Voluptate consectetur aute excepteur est cillum nisi esse
+            sit ad non aliquip consectetur quis voluptate. Dolor mollit mollit
+            elit reprehenderit est. Tempor eu magna mollit officia ea aliquip
+            exercitation minim laborum anim veniam ut.
+          </p>
+        </div>
+        <div className="my-10">
+          <h1 className="text-white text-3xl">Event Rules</h1>
+          <p className="text-white font-light mt-3">{event?.rules}</p>
+        </div>
+        <div className="my-10 space-y-10 md:space-y-0 flex flex-col md:flex-row justify-between">
+          <div className="flex flex-col">
+            <div className="items-center space-x-1 hidden md:inline-flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 text-white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 8.25H9m6 3H9m3 6l-3-3h1.5a3 3 0 100-6M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <p className="text-white">Entry Fee: Rs. {event?.entryFee}</p>
+            </div>
+            <button className="text-white hidden md:block mt-2 px-3 py-2 bg-red-500 rounded-lg">
+              Register Now
+            </button>
+            <button className="text-white md:hidden mt-2 px-3 py-2 bg-red-500 rounded-lg">
+              Register Now for Rs. {event?.entryFee}
+            </button>
+          </div>
+          <div className="text-white">
+            <h1>Student Coordinators</h1>
+            {event?.studentCo.map((student, index) => (
+              <p key={index} className="font-light">
+                {index + 1}) {student}
+              </p>
+            ))}
+          </div>
+          <div className="text-white">
+            <h1>Staff Coordinators</h1>
+            {event?.staffCo.map((staff, index) => (
+              <p key={index} className="font-light">
+                {index + 1}) {staff}
+              </p>
+            ))}
           </div>
         </div>
       </div>
