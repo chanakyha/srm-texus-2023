fd5bcf543c0fcc983659d070776ab94bdc592842
Surendar PD <80924716+surendar-pd@users.noreply.github.com>
2023-03-09 10:59:34 +0530

    fixed carousel

--- components/TechnicalEvents.jsx
+++ components/TechnicalEvents.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TechnicalEvents = () => {
     return (
         <div className="card">
-                            <h2 className="">Technical<br/> Events</h2>
+                            <h2 className="flex flex-col -space-y-3"><span>Technical</span><span>Events</span></h2>
                             <i className="fas fa-arrow-right"></i>
                             <p>Register Now</p>
                             <div className="pic"></div>
--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3-alpha-sig.figma.com',
+        pathname: '/**'
+      },
+    ],
+  },
 }
 
