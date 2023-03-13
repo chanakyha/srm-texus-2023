a322673a385a49c75e8bfaf05fccfaa63a6cbdc1
Chanakyha <vetrichanakyha2003@gmail.com>
2023-03-13 11:56:21 +0530

    fixed admin

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,23 @@
-import Head from "next/head";
-import Landing from "../components/Landing";
-import Tagline from "../components/Tagline";
-import EventDetails from "../components/EventDetails";
-import Carousel from "../components/Carousel";
-import Sponsors from "../components/Sponsors";
+import { useRouter } from "next/router";
+import React from "react";
 
-const Home = () => {
+function Admin() {
+  const router = useRouter();
   return (
-    <div className="flex min-h-screen bg-black w-full">
-      <Head>
-        <title>SRM Texus</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="w-full bg-black">
-        <Landing />
-        <Carousel />
-        <Tagline />
-        <EventDetails />
-        <Sponsors/>
-      </main>
+    <div className="min-h-[calc(100vh-330px)] mx-auto w-3/4 flex flex-col justify-evenly items-center bg-[#222222] rounded-lg">
+      <h1 className="text-white text-4xl font-bold">ADMIN</h1>
+      <div className="flex flex-col md:flex-row justify-evenly w-2/3 items-stretch gap-4 md:w-full">
+        <button
+          onClick={() => router.push("/admin/add-event")}
+          className="button"
+        >
+          Add Events
+        </button>
+        <button className="button">View Registrations</button>
+        <button className="button">Log out</button>
+      </div>
     </div>
   );
-};
+}
 
-export default Home;
+export default Admin;
