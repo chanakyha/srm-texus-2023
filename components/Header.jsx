bea3b972d3e2a4900965025aa2dcd34e168be106
Chanakyha <vetrichanakyha2003@gmail.com>
2023-03-13 10:57:16 +0530

    CodeAdmin

--- components/Header.jsx
+++ components/Header.jsx
@@ -83,8 +83,8 @@ const Header = () => {
             className="w-6 h-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M6 18L18 6M6 6l12 12"
             />
           </svg>
