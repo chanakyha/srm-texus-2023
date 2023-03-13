b1315d7550db3b6c64712526d0292cc2daa20a58
Sai Rohit <sairohit11.s@gmail.com>
2023-03-13 15:30:44 +0530

    Schedule page

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -9,47 +9,47 @@ import "swiper/css/scrollbar";
 import "swiper/css/autoplay";
 
 export default function Slider() {
-return (
+  return (
     <div className="md:px-12 mx-auto flex justify-center items-center">
-    <Swiper
+      <Swiper
         className="flex items-center align-items justify-center bg-transparent md:rounded-md"
         modules={[Pagination, Scrollbar, A11y, Autoplay]}
         slidesPerView={1}
         loop={true}
         autoplay={{
-        delay: 2000,
-        disableOnInteraction: true,
+          delay: 2000,
+          disableOnInteraction: true,
         }}
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => console.log(swiper)}
-    >
+      >
         <SwiperSlide className="flex justify-center items-center bg-transparent">
-        <div className="img-container flex items-center justify-center h-56 md:h-[36rem] bg-transparent">
+          <div className="img-container flex items-center justify-center h-56 md:h-[36rem] bg-transparent">
             <Image
-            className="object-cover w-full object-bottom rounded-3xl"
-            src={require("../assets/images/demo1.jpg")}
-            alt="slide one"
+              className="object-cover w-full object-bottom rounded-3xl"
+              src={require("../assets/images/demo1.jpg")}
+              alt="slide one"
             ></Image>
-        </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide className="flex justify-center items-center bg-transparent">
-        <div className="img-container flex items-center justify-center h-56 md:h-[36rem] bg-transparent">
+          <div className="img-container flex items-center justify-center h-56 md:h-[36rem] bg-transparent">
             <Image
-                className="object-cover w-full object-bottom rounded-3xl"
-                src={require("../assets/images/demo2.jpg")}
+              className="object-cover w-full object-bottom rounded-3xl"
+              src={require("../assets/images/demo2.jpg")}
             ></Image>
-        </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide className="flex justify-center items-center bg-transparent">
-        <div className="img-container flex items-center justify-center h-56 md:h-[36rem] bg-transparent">
+          <div className="img-container flex items-center justify-center h-56 md:h-[36rem] bg-transparent">
             <Image
-                className="object-cover w-full object-bottom rounded-3xl"
-                src={require("../assets/images/demo3.jpg")}
-                alt="slide one"
+              className="object-cover w-full object-bottom rounded-3xl"
+              src={require("../assets/images/demo3.jpg")}
+              alt="slide one"
             ></Image>
-        </div>
+          </div>
         </SwiperSlide>
-    </Swiper>
+      </Swiper>
     </div>
-);
+  );
 }
--- pages/schedule.jsx
+++ pages/schedule.jsx
@@ -1,44 +1,112 @@
-import React from 'react'
+import React, { useCallback, useRef, useState } from "react";
+import { technicalEvents } from "../assets/events";
+import Link from "next/link";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
 
 function Schedule() {
-    const events=[1,2,3,4,5,6,7,8]
-  return (
-    <div className='w-full h-full bg-black font-sniglet text-white'>
-        <div className='p-4 md:p-16'>
-            <div className='max-w-6xl lg:mx-auto'>
-                <h1 className="text-3xl my-7 ">Schedule</h1>
-                <div className='grid grid-cols-2'>
-                    <div><p className="text-2xl mb-5">21 April</p></div>
-                    <div class='place-self-end mb-3'> 
-                        <div className='inline-block'> 
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:text-sky-400 mr-4">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-                            </svg>
-                        </div>
-                        <div className='inline-block'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:text-sky-400">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                            </svg>
-                        </div>
-                    </div> 
-                </div>             
-            </div>
-            <div className='bg-[#1B1B1B] max-w-6xl mx-auto lg:mx-auto p-8 rounded-md flex flex-wrap items-strech justify-center'>
-                {events.map((event,index)=> (
-                    <div className='h-64 w-48 m-8 p-3 relative items-end bg-[#179EBB] text-white rounded-md'>
-                        <div className='absolute bottom-2  w-48 left-0 text-center mx-auto'>
-                            <p>Mindwrap</p>
-                            <p>8:00 AM</p>
-                        </div>
-                    </div>
-                ))}
+  const [date, setDate] = useState(1);
+  const swiperRef = useRef();
 
+  return (
+    <div className='w-full h-full bg-black text-white  bg-[url("../assets/images/bgoverlay.png")] mt-8 font-[Montserrat]'>
+      <div className="p-4 md:p-16">
+        <div className="max-w-6xl lg:mx-auto">
+          <h1 className="text-6xl my-7 font-montserrat">Schedule</h1>
+
+          <div className="max-w-6xl h-[5vh] flex justify-center items-center text-4xl mb-4">
+            <button onClick={() => swiperRef.current?.slidePrev()}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-10 h-10 mr-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 19.5L8.25 12l7.5-7.5"
+                />
+              </svg>
+            </button>
+
+            <div className="self-center mb-[8px]">
+              {date % 2 == 0 ? "22 April" : "21 April"}
             </div>
+
+            <button onClick={() => swiperRef.current?.slideNext()}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-10 h-10 ml-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-        
+        <Swiper
+          modules={[Pagination, Scrollbar, A11y, Autoplay, Navigation]}
+          slidesPerView={1}
+          onSlideChange={() => setDate(date + 1)}
+          onSwiper={(swiper) => console.log(swiper)}
+          onBeforeInit={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          className="flex items-center justify-center max-w-6xl rounded-2xl"
+        >
+          <SwiperSlide className="rounded-2xl sm:py-8 bg-[#222222]">
+            <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4">
+              {technicalEvents
+                .filter((dateValue) => dateValue.date == "21-04-2023")
+                .map((event) => (
+                  <Link
+                    href={`events/${event.id}`}
+                    className="h-24 w-11/12 sm:w-40 sm:h-64 lg:w-48 m-4 p-3 relative items-end bg-[#179EBB] text-white rounded-md flex justify-self-center self-center items-center justify-center"
+                  >
+                    <div className="w-48 left-0 text-center mx-auto">
+                      <p className="text-2xl">{event.eventName}</p>
+                      <p>{event.time}</p>
+                    </div>
+                  </Link>
+                ))}
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="bg-[#222222]  rounded-2xl sm:py-8">
+            <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4">
+              {technicalEvents
+                .filter((dateValue) => dateValue.date == "22-04-2023")
+                .map((event) => (
+                  <Link
+                    href={`events/${event.id}`}
+                    className="h-24 w-11/12 sm:w-40 sm:h-64 lg:w-48 m-4 p-3 relative items-end bg-[#179EBB] text-white rounded-md flex justify-self-center self-center items-center justify-center"
+                  >
+                    <div className="w-48 left-0 text-center mx-auto">
+                      <p className="text-2xl">{event.eventName}</p>
+                      <p>{event.time}</p>
+                    </div>
+                  </Link>
+                ))}
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
-  )
-    
+  );
 }
 
-export default Schedule
+export default Schedule;
